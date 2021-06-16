a730f3dcf58303b2c909a9dac12c4fd62ee10d1e
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-06-16 14:36:57 -0400

    Add androidx action bar

--- app/src/main/java/reci/proca/MainActivity.kt
+++ app/src/main/java/reci/proca/MainActivity.kt
@@ -1,11 +1,31 @@
 package reci.proca
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+
+    /**
+     * Handle the top-right menu clicks
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+//            R.id.menu_settings -> startActivity(Intent(this, SettingsActivity::class.java))
+        }
+        return true
     }
 }
