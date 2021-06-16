b2353d859d1b57ef5e779e160f81e4c13ddb4586
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-06-16 15:06:28 -0400

    fix action bar for prefs

--- app/src/main/java/reci/proca/pref/SettingsActivity.kt
+++ app/src/main/java/reci/proca/pref/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package reci.proca.pref
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import reci.proca.R
@@ -9,16 +10,32 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_settings)
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        supportActionBar?.apply {
+            title = getString(R.string.pref_settings_bar_title)
+            setDisplayHomeAsUpEnabled(true)
+        }
 
         setTheme(R.style.Theme_Reciproca)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
