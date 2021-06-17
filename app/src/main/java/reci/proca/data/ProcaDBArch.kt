9694ee8f49473e9d45ddefd3a371540f89fd0780
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-06-17 16:50:22 -0400

    Intro to pdbarch

---
+++ app/src/main/java/reci/proca/data/ProcaDBArch.kt
@@ -0,0 +1,52 @@
+@file:Suppress("unused", "ArrayInDataClass")
+
+package reci.proca.data
+
+import androidx.room.*
+
+/*
+ * The database design architecture
+ */
+
+@Entity
+data class Step(
+    @PrimaryKey val stepID: Int,
+    val description: String,
+    val annotation: String
+)
+
+
+@Entity
+data class Picture(
+    @PrimaryKey val pictureID: Int,
+    val fullFile: String,
+    val thumbFile: String,
+    val timestamp: Int,
+    val smallThumb: ByteArray
+)
+
+@Entity
+data class LogEntry(
+    @PrimaryKey val entryID: Int,
+    val timestamp: Int,
+    val modified: Int,
+    val recipeID: Int
+)
+
+@Entity
+data class LogEntryMemo(
+    @PrimaryKey val memoID: Int,
+    val logID: Int,
+    val rating: Int,
+    val description: String,
+)
+
+data class LogWithMemo(
+    @Embedded val entry: LogEntry,
+    @Relation(
+        parentColumn = "entryID",
+        entityColumn = "logID"
+    )
+    val memo: LogEntryMemo
+)
+
