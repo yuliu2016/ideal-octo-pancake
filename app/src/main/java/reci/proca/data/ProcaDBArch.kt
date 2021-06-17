3f59a73e0fccf95bd8b61959e3307fb424612bfd
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-06-17 18:32:43 -0400

    Proca!!!

--- app/src/main/java/reci/proca/data/ProcaDBArch.kt
+++ app/src/main/java/reci/proca/data/ProcaDBArch.kt
@@ -13,7 +13,9 @@ data class Step(
     @PrimaryKey val stepID: Int,
     val description: String,
     val annotation: String,
-    val pictureID: Int
+    val pictureID: Int,
+    val entryID: Int,
+    val isReference: Boolean
 )
 
 
@@ -31,23 +33,15 @@ data class LogEntry(
     @PrimaryKey val entryID: Int,
     val timestamp: Int,
     val modified: Int,
-    val recipeID: Int
+    val isDraft: Boolean,
+    val referenceID: Int,
+    val selfRating: Int,
+    val annotation: String,
 )
+
 
 @Entity
-data class LogEntryMemo(
-    @PrimaryKey val memoID: Int,
-    val logID: Int,
-    val rating: Int,
-    val description: String,
-)
+data class ReferenceEntry(
+    @PrimaryKey val refID: Int,
 
-data class LogWithMemo(
-    @Embedded val entry: LogEntry,
-    @Relation(
-        parentColumn = "entryID",
-        entityColumn = "logID"
     )
-    val memo: LogEntryMemo
-)
-
