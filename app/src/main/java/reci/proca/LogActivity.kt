c661f8a7ab58b64d70f249ed45b90f0515dc00c6
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-06-19 15:44:05 -0400

    Add log menu

--- app/src/main/java/reci/proca/LogActivity.kt
+++ app/src/main/java/reci/proca/LogActivity.kt
@@ -2,10 +2,23 @@ package reci.proca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.let { it.title = "Squamous" }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.log_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return false
     }
 }
